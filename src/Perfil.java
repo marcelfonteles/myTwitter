071c95c20274115273e98417a584ec1298c27dad
marcelfonteles <marcelfonteles@gmail.com>
2018-07-01 09:44:01 -0300

    commit inicial

---
+++ src/Perfil.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+
+public class Perfil {
+	private String usuario;
+	private Vector<Perfil> seguidos;
+	private Vector<Perfil> seguidores;
+	private Vector<Tweet> timeline;
+
+}
