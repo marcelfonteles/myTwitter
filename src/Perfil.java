d715bedfccfd3298851fcee9c81f8d256331f964
marcelfonteles <marcelfonteles@gmail.com>
2018-07-01 10:01:38 -0300

    chomps of code for abstract class Perfil

--- src/Perfil.java
+++ src/Perfil.java
@@ -4,6 +4,41 @@ public class Perfil {
 	private String usuario;
 	private Vector<Perfil> seguidos;
 	private Vector<Perfil> seguidores;
-	private Vector<Tweet> timeline;
-
+	private Vector<Tweet> timeline; // Erro pq ainda não existe uma classe Tweet.
+	private boolean ativo;
+	
+	// CONSTRUTOR
+	public Perfil(String usuario) {
+		this.usuario = usuario;
+	}
+	public void addSeguido(Perfil usuario) {
+		
+	}
+	public void addSeoguidor(Perfil usuario) {
+		
+	}
+	public void addTweet(Tweet tweet) {
+		
+	}
+	public void setUsuario(String usuario) {
+		
+	}
+	public String getUsuario() { // Erro pq não tá retornando uma String ainda.
+		return "Teste";
+	}
+	public Vector<Perfil> getSeguidos() { // Erro pq não tá retornando um Vector ainda.
+		
+	}
+	public Vector<Perfil> getSeguidores(){ // Erro pq não tá retornando um Vector ainda.
+		
+	}
+	public Vector<Tweet> getTimeline(){
+		
+	}
+	public void setAtivo(boolean valor) {
+		
+	}
+	public boolean isAtivo() {
+		return true;
+	}
 }
